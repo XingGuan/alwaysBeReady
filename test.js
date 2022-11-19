ad20d2d5dfec2c48ca1ca82d92539c06b37a9ca2
guan.xing <1456725324@qq.com>
2022-11-20 00:39:51 +0800

    更新

---
+++ test.js
@@ -0,0 +1,52 @@
+linkidDb.getCollection('IDENTITY_LABEL').remove({"_id": ObjectId("61924e5fdf96a50006daf377")},{$setOnInsert:
+    {
+        "_id" : ObjectId("61924e5fdf96a50006daf377"),
+        "jobStatus" : 0,
+        "jobRoleStatus" : 0,
+        "corn" : "0 51 0 * * ?",
+        "name" : "xxxxxxxxxx",
+        "code" : "10240",
+        "roleIds" : [ ],
+        "from" : "5c662f3e9a4f79b8f5000001",
+        "firstCreateUser" : "60e54636ecc62b0006eb2f07",
+        "rule" : "{\"andOr\":true,\"filedName\":\"id\",\"filedValue\":\"5d70ab62f409090001c378a0\",\"not\":false,\"regex\":false}",
+        "organizationId" : "RJXZZZ",
+        "organizationName" : "行政组织",
+        "show" : true,
+        "labelTypeIds" : [
+            "619249d4213ae300067a1008"
+        ],
+        "showStatus" : "public",
+        "noEdit" : false,
+        "eventType" : false,
+        "createdTime" : ISODate("2021-11-15T12:11:11.015Z"),
+        "updatedTime" : ISODate("2022-02-23T05:56:36.268Z"),
+        "isDeleted" : false
+    }
+},{upsert: true});
+linkidDb.getCollection('IDENTITY_LABEL').remove({"_id": ObjectId("61924e5fdf96a50006daf377")},{$setOnInsert:
+    {
+        "_id" : ObjectId("61924e5fdf96a50006daf377"),
+        "jobStatus" : 0,
+        "jobRoleStatus" : 0,
+        "corn" : "0 51 0 * * ?",
+        "name" : "yyyyyyyy",
+        "code" : "10240",
+        "roleIds" : [ ],
+        "from" : "5c662f3e9a4f79b8f5000001",
+        "firstCreateUser" : "60e54636ecc62b0006eb2f07",
+        "rule" : "{\"andOr\":true,\"filedName\":\"id\",\"filedValue\":\"5d70ab62f409090001c378a0\",\"not\":false,\"regex\":false}",
+        "organizationId" : "RJXZZZ",
+        "organizationName" : "行政组织",
+        "show" : true,
+        "labelTypeIds" : [
+            "619249d4213ae300067a1008"
+        ],
+        "showStatus" : "public",
+        "noEdit" : false,
+        "eventType" : false,
+        "createdTime" : ISODate("2021-11-15T12:11:11.015Z"),
+        "updatedTime" : ISODate("2022-02-23T05:56:36.268Z"),
+        "isDeleted" : false
+    }
+},{upsert: true});
